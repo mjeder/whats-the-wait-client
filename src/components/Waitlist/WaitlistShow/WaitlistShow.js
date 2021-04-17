3ca37c7ec4a6948a0baa3f06b00cfe54039232f0
mjeder <matteder96@gmail.com>
2021-04-17 15:54:24 -0500

    Add table of guests in waitlist

--- src/components/Waitlist/WaitlistShow/WaitlistShow.js
+++ src/components/Waitlist/WaitlistShow/WaitlistShow.js
@@ -1,7 +1,10 @@
-import React, { Component } from 'react'
+import React, { Component, Fragment } from 'react'
 import Spinner from 'react-bootstrap/Spinner'
 import { withRouter, Redirect, Link } from 'react-router-dom'
-import Button from 'react-bootstrap/Button'
+// import Button from 'react-bootstrap/Button'
+import Table from 'react-bootstrap/Table'
+import Moment from 'react-moment'
+import Image from 'react-bootstrap/Image'
 import { waitlistShow, waitlistDelete } from '../../../api/waitlist'
 
 class WaitlistShow extends Component {
@@ -22,8 +25,7 @@ class WaitlistShow extends Component {
       .then(res => this.setState({ waitlist: res.data.waitlist }))
       .then(() => msgAlert({
         heading: 'Success!',
-        message: 'I present to you, your waitlist.',
-        variant: 'success'
+        variant: 'light'
       }))
       .catch(error => {
         msgAlert({
@@ -41,7 +43,7 @@ class WaitlistShow extends Component {
       .then(res => msgAlert({
         heading: 'Success!',
         message: 'Waitlist has been deleted.',
-        variant: 'success'
+        variant: 'light'
       }))
       .catch(error => {
         msgAlert({
@@ -52,6 +54,7 @@ class WaitlistShow extends Component {
       })
   }
   render () {
+    let guestJsx
     const { waitlist, deleted } = this.state
     if (!waitlist) {
       return (
@@ -59,26 +62,77 @@ class WaitlistShow extends Component {
           <span className="sr-only">Loading...</span>
         </Spinner>
       )
+    } else {
+      guestJsx = (
+        waitlist.guests.map(guest => (
+          <Fragment key={guest._id}>
+            <tr>
+              <td>{waitlist.guests.indexOf(guest) + 1}</td>
+              <td><Moment format='hh:mm A'>{guest.createdAt}</Moment></td>
+              <td>{guest.name}</td>
+              <td>{guest.phone}</td>
+              <td>{guest.count}</td>
+              <td>{guest.notes}</td>
+              <td>{guest.quote}</td>
+              <td>{guest.table}</td>
+              <td>
+                <Link to={`/guests/${waitlist._id}/${guest._id}`} key={guest._id}>
+                  <Image
+                    src="https://icongr.am/entypo/check.svg?size=25&color=29a745"
+                    alt="Sit"
+                    className="mr-3"
+                  ></Image>
+                </Link>
+                <Link to={`/guests/${waitlist._id}/${guest._id}`} key={guest._id}>
+                  <Image
+                    src="https://icongr.am/entypo/trash.svg?size=25&color=dc3545"
+                    alt="Delete"
+                    className="mr-3"
+                  ></Image>
+                </Link>
+                <Link to={`/guests/${waitlist._id}/${guest._id}`} key={guest._id}>
+                  <Image
+                    src="https://icongr.am/entypo/pencil.svg?size=25&color=047bff"
+                    alt="Edit"
+                    className="mr-3"
+                  ></Image>
+                </Link>
+                <Link to={'#'} key={guest._id}>
+                  <Image
+                    src="https://icongr.am/entypo/message.svg?size=25&color=333333"
+                    alt="Text"
+                  ></Image>
+                </Link>
+              </td>
+            </tr>
+          </Fragment>
+        ))
+      )
     }
+
     if (deleted) {
       return <Redirect to="/waitlists" />
     }
 
-    const guestJsx = waitlist.guests.map(guest => (
-      <div key={waitlist.guests._id}>
-        <li>{waitlist.guests}</li>
-      </div>
-    ))
-
     return (
-      <div>
-        <h3>{waitlist.name}</h3>
-        <h5>{guestJsx}</h5>
-        <Button onClick={this.handleDelete}>Delete Waitlist</Button>
-        <Link to={`/waitlists/${waitlist._id}/edit`}>
-          <Button renderas='button'>Edit Waitlist</Button>
-        </Link>
-      </div>
+      <Table striped bordered hover className="text-center mt-5">
+        <thead>
+          <tr>
+            <th>#</th>
+            <th>Added</th>
+            <th>Name</th>
+            <th>Phone</th>
+            <th>Guests</th>
+            <th>Notes</th>
+            <th>Quote</th>
+            <th>Table</th>
+            <th>Actions</th>
+          </tr>
+        </thead>
+        <tbody>
+          {guestJsx}
+        </tbody>
+      </Table>
     )
   }
 }
