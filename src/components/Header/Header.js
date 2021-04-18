651605236cb944e3cca57765cfc8a103fa5b16d2
mjeder <matteder96@gmail.com>
2021-04-17 23:23:58 -0500

    Remove create from nav

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -4,8 +4,7 @@ import Navbar from 'react-bootstrap/Navbar'
 
 const authenticatedOptions = (
   <Fragment>
-    <Nav.Link href="#create-waitlist">New Waitlist</Nav.Link>
-    <Nav.Link href="#waitlists">My Waitlists</Nav.Link>
+    <Nav.Link href="#waitlists">Waitlists</Nav.Link>
     <Nav.Link href="#change-password">Change Password</Nav.Link>
     <Nav.Link href="#sign-out">Sign Out</Nav.Link>
   </Fragment>
